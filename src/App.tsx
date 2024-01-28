d66060ecf1633c59b49f6d33ab388a8214d87103
tural.shkrov <tural.shkrov@gmail.com>
2024-01-28 22:12:50 +0400

    feat: router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Search from './pages/search/Search';
+import Blogs from './pages/blogs/Blogs';
+import Profile from './pages/profile.tsx/Profile';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { NavLink } from "react-router-dom";
 import { MdHomeFilled, MdSearch, MdFormatQuote, MdPerson } from "react-icons/md";
 
 export default function Navbar() {
@@ -6,33 +6,33 @@ export default function Navbar() {
     <header className="navbar navbar-mobile navbar-mobile-dark">
       <div className="container">
         <div className="nav-logo">
-          <a href="/">Alexandria</a>
+          <span>Alexandria</span>
         </div>
         <nav className="nav-menu nav-menu-mobile">
           <ul className="nav-list-mobile">
             <li>
-              <a href="/" className="nav-link active">
+              <NavLink to='/' className='nav-link'>
                 <MdHomeFilled className='nav-mobile-icon'/>
                 <span>Home</span>
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a href="/" className="nav-link">
+              <NavLink to="/search" className="nav-link">
                 <MdSearch className='nav-mobile-icon'/>
                 <span>Search</span>
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a href="/" className="nav-link">
+              <NavLink to="/blogs" className="nav-link">
                 <MdFormatQuote className='nav-mobile-icon'/>
                 <span>Blogs</span>
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a href="/" className="nav-link">
+              <NavLink to="/profile" className="nav-link">
                 <MdPerson className='nav-mobile-icon'/>
                 <span>Profile</span>
-              </a>
+              </NavLink>
             </li>
           </ul>
         </nav>
