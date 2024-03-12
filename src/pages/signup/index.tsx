9b11bed5862acfc54ec0401f17ecdad687ca9306
tural.shkrov <tural.shkrov@gmail.com>
2024-03-12 13:02:26 +0400

    feat: sign up validation

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,41 +1,118 @@
-
 import Button from "@/shared/components/button";
-import "./index.scss";
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form"
+import { useState } from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import "./index.scss";
+
+type Form = {
+  name: string,
+  username: string,
+  email: string,
+  password: string
+}
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Form>();
+  const onSubmit: SubmitHandler<Form> = (data) => console.log(data);
+  const [ showPassword, setshowPassword ] = useState(false);
+
   return (
     <div className="page page-without-navbar page-vertical-center">
       <div className="container mb-md-0">
         <div className="py-3 register-content text-center">
           <h1 className="logo-title">Alexandria</h1>
           <h1 className="mt-2">Create Account</h1>
-          <form className="register-form form-control mt-2">
+          <form onSubmit={handleSubmit(onSubmit)} className="register-form form-control mt-2">
             <div className="form-item">
               <label htmlFor="name">Your Name</label>
-              <input type="text" id="name"/>
+              <input
+                type="text"
+                id="name"
+                {...register("name", { required: true, maxLength: 30, pattern: /^[a-zA-Z\s]/ })}
+              />
+              {errors.name && errors.name.type === "required" && (
+                <span className="text-danger font-sm">Name is required</span>
+              )}
             </div>
             <div className="form-item">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username"/>
+              <input
+                id="username"
+                type="text"
+                {...register("username", { required: true, minLength: 6, maxLength: 30, pattern: /^[a-zA-Z]+$/ })}
+              />
+              {errors.username && errors.username.type === "required" && (
+                <span className="text-danger font-sm">Username is required</span>
+              )}
+              {errors.username && errors.username.type === "minLength" && (
+                <span className="text-danger font-sm">Username must conatin minimum 6 characters</span>
+              )}
+              {errors.username && errors.username.type === "pattern" && (
+                <span className="text-danger font-sm">Username must conatin only English letters</span>
+              )}
             </div>
             <div className="form-item">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email"/>
+              <input
+                id="email"
+                type="email"
+                {...register("email", {
+                  required: true,
+                  maxLength: 30,
+                  pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                })} />
+              {errors.email && errors.email.type === "required" && (
+                <span className="text-danger font-sm">Email is required</span>
+              )}
+              {errors.email && errors.email.type === "pattern" && (
+                <span className="text-danger font-sm">Email is not valid</span>
+              )}
             </div>
-            <div className="form-item">
+            <div className="form-item password-item">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password"/>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                {...register("password", { required: true,
+                  minLength: 8,
+                  maxLength: 30,
+                  pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/ })} 
+              />
+              <span 
+                className="show-password"
+                onClick={() => setshowPassword(!showPassword)}>
+                {showPassword ? <IoMdEyeOff /> : <IoMdEye />}
+              </span>
+              {errors.password && errors.password.type === "required" && (
+                <span className="text-danger font-sm">Password is required</span>
+              )}
+              {errors.password && errors.password.type === "pattern" && (
+                <span className="text-danger font-sm">Password must contain:<br/>
+                At least one lowercase letter<br/>
+                At least one uppercase letter<br/>
+                At least one number<br/>
+                At least one special character
+                </span>
+              )}
+              {errors.password && errors.password.type === "minLength" && (
+                <span className="text-danger font-sm">Password must contain minimum 8 characters</span>
+              )}
             </div>
             <p className="mt-2">
               By creating an account, you agree to the Alexandria <Link to='/terms'>Terms of Service</Link> and <Link to='/privacy'>Privacy Policy</Link>
             </p>
             <Button
+              type="submit"
               className="mt-2">
               Create Account
             </Button>
             <p className="mt-3">
-              Already have an account? <Link to='/sign'>Sign in</Link>
+              Already have an account? <Link to='/login'>Sign in</Link>
             </p>
           </form>
         </div>
--- src/shared/components/button/index.tsx
+++ src/shared/components/button/index.tsx
@@ -3,6 +3,7 @@ import "./index.scss";
 type ButtonStyle = 'solid' | 'outline' | 'link';
 type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xxl';
 type ButtonColor = 'primary' | 'dark' | 'light';
+type ButtonType = "button" | "submit" | "reset" | undefined
 
 type ButtonProps = {
   size?: ButtonSize,
@@ -10,13 +11,15 @@ type ButtonProps = {
   style?: ButtonStyle,
   children?: React.ReactNode,
   className?: string,
+  type?: ButtonType,
   onClick?: () => void,
 }
 
-export default function Button({ size, color='primary', style='solid', className='', children, onClick }: ButtonProps) {
+export default function Button({ size, color='primary', style='solid', className='', type='button', children, onClick }: ButtonProps) {
   className += size ? ' btn-' + size : ' ';
   return (
-    <button 
+    <button
+      type={type}
       className={`${className} btn-${style}-${color}`} 
       onClick={() => onClick?.()}>
       {children}
