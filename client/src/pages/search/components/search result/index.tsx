d92b3a4a4dcec8ee2a1bd010ed4c0c45eb5059cc
tural.shkrov <tural.shkrov@gmail.com>
2024-03-30 22:16:24 +0400

    feat: search

--- client/src/pages/search/components/search result/index.tsx
+++ client/src/pages/search/components/search result/index.tsx
@@ -8,10 +8,10 @@ const SearchResult = () => {
   const searchKeyword = useAppSelector(state => state.SearchSlice.searchKeyword);
   return (
     <div className="search-result">
-      <h1 className="mb-3 mt-1 fw-regular">{`Search "${searchKeyword}"`}</h1>
       {
         (searchKeyword.length > 2) &&
         <>
+          <h1 className="mb-3 mt-1 fw-regular">{`Search "${searchKeyword}"`}</h1>
           <BookSearchResult />
           <AuthorSearchResult />
           <ListSearchResult />
