4eea2d02c6d42844bd3823175d570d05cca6c87f
tural.shkrov <tural.shkrov@gmail.com>
2024-04-06 22:23:14 +0400

    fix: update profile

--- server/validations/user/usernameValidationRules.js
+++ server/validations/user/usernameValidationRules.js
@@ -4,7 +4,7 @@ const usernameValidationRules = () => [
   body('name')
     .notEmpty().withMessage('Name is required')
     .isLength({ max: 64 }).withMessage('Name  can be at most 64 characters long')
-    .isAlpha.withMessage('Name can only contain letters and spaces'),
+    .isAlpha().withMessage('Name can only contain letters and spaces'),
   body('username')
     .notEmpty().withMessage('Username is required')
     .isLength({ min: 3, max: 32}).withMessage('Username can be at least 3 and at most 32 characters long')
