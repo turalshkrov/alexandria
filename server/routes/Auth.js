5c9a77d84c9e2553dde4d99a9f07ea07a12b23ed
tural.shkrov <tural.shkrov@gmail.com>
2024-04-07 23:47:23 +0400

    fix: authentication

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const UserRole = require('../models/UserRole');
+const authenticationToken = require('../middlewares/auth/authenticationToken');
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
@@ -28,8 +29,22 @@ router.post('/login', async (req, res) => {
     res.status(200).json({ 
       message: "Login success",
       token: token,
-      userId: user._id,
       role: userRole.role,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/getMe', authenticationToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+    const userRole = await UserRole.findOne({ userId: user._id });
+    if (!user) {
+      return res.status(404).json({ message: "Couldn't find your account" });
+    }
+    res.status(200).json({
+      user, userRole
     });
   } catch (error) {
     res.status(500).json(error);
